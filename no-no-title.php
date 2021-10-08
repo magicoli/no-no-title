bd10378f003039d1a8441eb44b977e555d6ac7c8
Olivier van Helden <gudule@speculoos.world>
2021-10-08 17:09:21 -0400

    Initial commit

---
+++ no-no-title.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     No "(no title)"
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Replace '(no title)' in admin lists by an excerpt of the excerpt for posts with empty titles
+ * Author:          Magiiic
+ * Author URI:      https://magiiic.com/
+ * Text Domain:     no-no-title
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         No_No_Title
+ */
+
+// Your code starts here.
